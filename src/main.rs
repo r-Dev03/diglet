665c4cf947704ee3d262be41fd0b7b34274c5f0e
Ribbal Hussain <ribbalh0@gmail.com>
2026-05-21 10:34:54 -0500

    more testing

--- src/main.rs
+++ src/main.rs
@@ -10,29 +10,29 @@ use leptos::prelude::*;
 // and their properties.
 
 /// Shows progress toward a goal.
-#[component]
-fn ProgressBar(
-    // Marks this as an optional prop. It will default to the default
-    // value of its type, i.e., 0.
-    #[prop(default = 100)]
-    /// The maximum value of the progress bar.
-    max: u16,
-    // Will run `.into()` on the value passed into the prop.
-    #[prop(into)]
-    // `Signal<T>` is a wrapper for several reactive types.
-    // It can be helpful in component APIs like this, where we
-    // might want to take any kind of reactive value
-    /// How much progress should be displayed.
-    progress: Signal<i32>,
-) -> impl IntoView {
-    view! {
-        <progress
-            max={max}
-            value=progress
-        />
-        <br/>
-    }
-}
+// #[component]
+// fn ProgressBar(
+//     // Marks this as an optional prop. It will default to the default
+//     // value of its type, i.e., 0.
+//     #[prop(default = 100)]
+//     /// The maximum value of the progress bar.
+//     max: u16,
+//     // Will run `.into()` on the value passed into the prop.
+//     #[prop(into)]
+//     // `Signal<T>` is a wrapper for several reactive types.
+//     // It can be helpful in component APIs like this, where we
+//     // might want to take any kind of reactive value
+//     /// How much progress should be displayed.
+//     progress: Signal<i32>,
+// ) -> impl IntoView {
+//     view! {
+//         <progress
+//             max={max}
+//             value=progress
+//         />
+//         <br/>
+//     }
+// }
 
 #[component]
 fn App() -> impl IntoView {
