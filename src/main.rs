cb3a7ad7fc68ac1d9383af63cf888f459ee877b9
Ribbal Hussain <ribbalh0@gmail.com>
2026-05-26 09:36:29 -0500

    messing around with mouse events

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod model;
 use leptos::{prelude::*, svg::view};
 use candle_core::{Device, Tensor, DType, Result, display};
+use leptos::logging::log;
 
 use leptos::prelude::*;
 
@@ -9,62 +10,26 @@ use leptos::prelude::*;
 // You'll see how doc comments can be used to document components
 // and their properties.
 
-/// Shows progress toward a goal.
-// #[component]
-// fn ProgressBar(
-//     // Marks this as an optional prop. It will default to the default
-//     // value of its type, i.e., 0.
-//     #[prop(default = 100)]
-//     /// The maximum value of the progress bar.
-//     max: u16,
-//     // Will run `.into()` on the value passed into the prop.
-//     #[prop(into)]
-//     // `Signal<T>` is a wrapper for several reactive types.
-//     // It can be helpful in component APIs like this, where we
-//     // might want to take any kind of reactive value
-//     /// How much progress should be displayed.
-//     progress: Signal<i32>,
-// ) -> impl IntoView {
-//     view! {
-//         <progress
-//             max={max}
-//             value=progress
-//         />
-//         <br/>
-//     }
-// }
+
 
 #[component]
 fn App() -> impl IntoView {
-    let (count, set_count) = signal(0);
-
-    let double_count = move || count.get() * 2;
-
     view! {
-        <button
-            on:click=move |_| {
-                *set_count.write() += 1;
-            }
-        >
-            "Click me"
-        </button>
-        <br/>
-        // If you have this open in CodeSandbox or an editor with
-        // rust-analyzer support, try hovering over `ProgressBar`,
-        // `max`, or `progress` to see the docs we defined above
-        <ProgressBar max=50 progress=count/>
-        // Let's use the default max value on this one
-        // the default is 100, so it should move half as fast
-        <ProgressBar progress=count/>
-        // Signal::derive creates a Signal wrapper from our derived signal
-        // using double_count means it should move twice as fast
-        <ProgressBar max=50 progress=Signal::derive(double_count)/>
+        <canvas 
+            on:mousedown=move |ev| log!("Down at: {}, {}", ev.x(), ev.y())
+            on:mouseup=move |ev| log!("Up at: {}, {}", ev.x(), ev.y())
+            on:mousemove=move |ev| log!("Move: {}, {}", ev.x(), ev.y())
+            width="280"
+            height="280"
+            style="border: 1px solid black;"
+        />
     }
 }
 
 fn main() {
   mount_to_body(App);
 }
+
 
 
 fn draw() {
